b55c71fa3165c54ee1e07dea5645b8965a7bc596
sayed <elsayedhossny90@gmail.com>
2026-09-18 19:20:55 +0300

    Enhance Profile

--- src/lib/serverUtilts.ts
+++ src/lib/serverUtilts.ts
@@ -1,11 +1,36 @@
+// "use server";
+// import { decode } from "next-auth/jwt";
+// import { cookies } from "next/headers";
+
+
+// export async function  getUserToken() {
+//     const encodedeToken= (await cookies()).get("next-auth.session-token")?.value;
+//     const decodedToken = await decode({token : encodedeToken ,secret :process.env.NEXTAUTH_SECRET ! });
+//     return decodedToken?.token;
+// }
+
+
+
 "use server";
+
 import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
+export async function getUserToken() {
+  const cookieStore = await cookies();
 
-export async function  getUserToken() {
-    const encodedeToken= (await cookies()).get("next-auth.session-token")?.value;
-    const decodedToken = await decode({token : encodedeToken ,secret :process.env.NEXTAUTH_SECRET ! });
-    return decodedToken?.token;
+  const encodedToken =
+    cookieStore.get("__Secure-next-auth.session-token")?.value ??
+    cookieStore.get("next-auth.session-token")?.value;
+
+  if (!encodedToken) {
+    return null;
+  }
+
+  const decodedToken = await decode({
+    token: encodedToken,
+    secret: process.env.NEXTAUTH_SECRET!,
+  });
+
+  return decodedToken?.token;
 }
-
